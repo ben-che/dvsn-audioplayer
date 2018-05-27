87742f39ebf4093febe89c380bbad1d371b2382b
benche <ben.che9@gmail.com>
2018-05-26 21:31:47 -0400

    importing components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Album from "./components/Album";
+import Artist from "./components/Artist";
+import SongList from "./components/SongList";
+import Visualization from './components/Visualization';
+import Controls from './components/Controls';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="artist-container"></div>
-        <div className="visualization-container"></div>
-        <div className="album-container"></div>
-        <div className="song-list-container"></div>
-        <div className="controls-container"></div>
+        <div className="artist-container"><Artist /></div>
+        <div className="visualization-container"><Visualization /></div>
+        <div className="album-container"><Album /></div>
+        <div className="song-list-container"><SongList /></div>
+        <div className="controls-container"><Controls /></div>
       </div>
     );
   }
