02df9061490930fb27468bca0fbb50b2ec44042d
benche <ben.che9@gmail.com>
2018-05-29 15:00:55 -0400

    npm react-icons added

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Controls.css';
+import Play from 'react-icons/lib/fa/play-circle';
 
 class Controls extends Component {
   render() {
     return (
       <div className="controls-body">
         {this.props.currentSong.name}
+        <div className=""><Play /></div>
       </div>
     );
   }
